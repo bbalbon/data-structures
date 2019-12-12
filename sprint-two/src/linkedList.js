c9efe908408a7602c57197b0a9df3e85784b06e9
Hack Reactor Student <student@hackreactor.com>
2019-12-12 15:51:04 -0800

    Finish Pseudo Code and implementing code
    Passed all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,49 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // Create Node
+    toAdd = Node(value);
+
+    if (!list.tail && !list.head) {
+      list.head = toAdd;
+      list.tail = toAdd;
+    }
+
+    // add node to list
+    list[value] = toAdd;
+    // set current tail node.next to point to new node we're adding
+    list.tail.next = toAdd;
+    // set new tail to be value added
+    list.tail = toAdd;
   };
 
   list.removeHead = function() {
+    //result variable to store current head
+    var result = list.head;
+    //delete current head
+    delete list[result.value];
+    //assign new head
+    list.head = result.next;
+    //return result with stored old head
+    return result.value;
   };
 
   list.contains = function(target) {
+
+
+    const recurse = (node) => {
+      if (node.value === target) {
+        return true;
+      } else if (!node.next) {
+        return false;
+      } else {
+        return recurse(node.next);
+      }
+
+    };
+
+    return recurse(list.head);
+
   };
 
   return list;
@@ -27,3 +64,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
