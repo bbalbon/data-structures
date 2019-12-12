96ffb1ec9882dfd7ef76395912727c571d4434a4
Bryce Balbon <bbalbon@gmail.com>
2019-12-11 21:05:46 -0800

    Complete Prorotypal prompts

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  let someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.top = 0;
+  someInstance.length = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function (value) {
+    this.top ++;
+    this.storage[this.top] = value;
+    this.length ++;
+  },
+  pop : function () {
+    if (this.length > 0) {
+      let result = this.storage[this.top];
+      delete this.storage[this.top];
+      this.top --;
+      this.length --;
+      return result;
+    }
+  },
+  size : function () {
+    return this.length;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.top = 0;
+  this.length = 0;
+
 };
+
+Stack.prototype.push = function () {
+  
+}
 
 
