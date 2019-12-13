55c2e25f35cac77ebe2d438255d7be8ed05df4b6
Hack Reactor Student <student@hackreactor.com>
2019-12-13 10:54:40 -0800

    Finish Graph data structures
    Passed all tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -20,25 +20,36 @@ Graph.prototype.contains = function(node) {
 Graph.prototype.removeNode = function(node) {
   //delete node
   //delete edges from all other nodes connected to node, recursion??
+  for (let key in this.storage[node]) {
+    this.removeEdge(node, key);
+  }
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   //go to fromNode
   //check in fromNode edges, if it has key toNode
-  return this.storage.fromNode.hasOwnProperty(toNode);
+  return this.storage[fromNode].hasOwnProperty(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode][toNode] = toNode;
+  this.storage[toNode][fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.storage[fromNode][toNode];
+  delete this.storage[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.storage) {
+    cb(key);
+  }
 };
 
 /*
