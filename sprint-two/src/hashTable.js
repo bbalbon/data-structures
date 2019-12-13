f5f0db467b78735efc5aedb1f98cea3c98ae72e2
Hack Reactor Student <student@hackreactor.com>
2019-12-13 15:57:08 -0800

    Pseudo code and implement hash insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,8 +5,18 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { // insert('Stephen', 'Tyler');
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //bucket variable to get bucket at index
+  var bucket = this._storage.get(index);
+  //if bucket is available
+  if (bucket !== undefined) {
+    bucket.push([k, v]);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
+  //push [k,v] into bucket
+  //else make new bucket [[k ,v]]
 };
 
 HashTable.prototype.retrieve = function(k) {
