5ecc27f4df32189c27df23939c5d33e831e5b926
Hack Reactor Student <student@hackreactor.com>
2019-12-13 21:45:56 -0800

    Working on contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -38,8 +38,17 @@ BinarySearchTree.prototype.insert = function (k) {
 };
 //.contains method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
 BinarySearchTree.prototype.contains = function (k) {
-  
-
+  let isTrue = false;
+  const recurse = (parent) => {
+    if (parent.value === k) {
+      isTrue = true;
+    } else if (parent.right && parent.right.value < k) {
+      recurse(parent.right);
+    } else if (parent.left && parent.left.value > k) {
+      recurse(parent.left);
+    }
+  };
+  return isTrue;
 };
 //.depthFirstLog method, which accepts a callback and executes it on every value contained in the tree.
 BinarySearchTree.prototype.depthFirstLog = function (callback) {
