c3332b7015be2eb3b325ba6ba6206d1e2f03fb3e
Hack Reactor Student <student@hackreactor.com>
2019-12-13 21:05:31 -0800

    Implement insert for Binary

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,49 @@
 var BinarySearchTree = function(value) {
+
+  //.left property, a binary search tree (BST) where all values are lower than the current value.
+  this.left = null;
+
+  //.right property, a BST where all values are higher than the current value.
+  this.right = null;
+
+  this.value = value;
+
+
+};
+
+
+
+//.insert method, which accepts a value and places it in the tree in the correct position.
+BinarySearchTree.prototype.insert = function (k) {
+  const obj = new BinarySearchTree(k);
+
+  const recurse = (parent) => {
+    if (parent.value < k) {
+      if (!parent.right) {
+        parent.right = obj;
+      } else {
+        recurse(parent.right);
+      }
+    } else {
+      if (!parent.left) {
+        parent.left = obj;
+      } else {
+        recurse(parent.left);
+      }
+    }
+  };
+
+  recurse(this);
+
+};
+//.contains method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+BinarySearchTree.prototype.contains = function (k) {
+  
+
+};
+//.depthFirstLog method, which accepts a callback and executes it on every value contained in the tree.
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+
 };
 
 
