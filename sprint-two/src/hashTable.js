bcb23536d071c0a84c81d0c97109bb816f801c9d
Hack Reactor Student <student@hackreactor.com>
2019-12-13 16:04:29 -0800

    Pseudo code part of limit X2 for insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,10 +3,12 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //this.counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v) { // insert('Stephen', 'Tyler');
   var index = getIndexBelowMaxForKey(k, this._limit);
+
   //bucket variable to get bucket at index
   var bucket = this._storage.get(index);
   //if bucket is available
@@ -17,6 +19,9 @@ HashTable.prototype.insert = function(k, v) { // insert('Stephen', 'Tyler');
   }
   //push [k,v] into bucket
   //else make new bucket [[k ,v]]
+  //this.counter ++
+  // if (counter > 0.75 * (this._limit))
+    // this._limit *= 2;
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -25,6 +30,7 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //this.counter --
 };
 
 
